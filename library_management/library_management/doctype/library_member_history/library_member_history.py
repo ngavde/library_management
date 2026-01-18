1400e4f0e82cf83e0d646645ad480032cabe0d0f
Nava <gavdenavnath.44@gmail.com>
2026-01-18 14:35:14 +0530

    librar changs

--- library_management/library_management/doctype/library_member_history/library_member_history.py
+++ library_management/library_management/doctype/library_member_history/library_member_history.py
@@ -5,12 +5,44 @@ import frappe
 from frappe.model.document import Document
 
 class LibraryMemberHistory(Document):
-	pass
-	# appending data in child table of same doctype
-	# def before_save(self):
-	# 	a = self.member_name
+	def validate(self):
+		# Ensure only one history record per member
+		if not self.is_new():
+			return
 
-	# 	self.append("transaction_history",{
-	# 		"article":self.member_first_name
-	# 	})
-			
+		existing_history = frappe.get_all("Library Member History",
+			filters={"member_name": self.member_name, "name": ["!=", self.name]})
+
+		if existing_history:
+			frappe.throw(f"History record already exists for member {self.member_name}")
+
+	@staticmethod
+	def get_or_create_history(member_name):
+		"""Get existing history record or create new one for a member"""
+		existing_history = frappe.get_all("Library Member History",
+			filters={"member_name": member_name}, limit=1)
+
+		if existing_history:
+			return frappe.get_doc("Library Member History", existing_history[0].name)
+		else:
+			# Create new history record
+			history_doc = frappe.new_doc("Library Member History")
+			history_doc.member_name = member_name
+			history_doc.save()
+			return history_doc
+
+	@staticmethod
+	def add_transaction_to_history(member_name, article=None, author=None, isbn=None, transaction_status=None, transaction_date=None):
+		"""Add a transaction line to the member's history"""
+		history_doc = LibraryMemberHistory.get_or_create_history(member_name)
+
+		history_doc.append("transaction_history", {
+			"article": article,
+			"author": author,
+			"isbn": isbn,
+			"transaction_status": transaction_status,
+			"transaction_date": transaction_date
+		})
+
+		history_doc.save()
+		return history_doc
--- library_management/library_management/doctype/library_member_history/test_library_member_history.py
+++ library_management/library_management/doctype/library_member_history/test_library_member_history.py
@@ -1,8 +1,52 @@
 # Copyright (c) 2022, Vtech Technologies and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 import unittest
+from library_management.library_management.doctype.library_member_history.library_member_history import LibraryMemberHistory
 
 class TestLibraryMemberHistory(unittest.TestCase):
-	pass
+	def setUp(self):
+		# Clean up any existing test data
+		frappe.db.rollback()
+		frappe.delete_doc_if_exists("Library Member History", "test-member-1", ignore_missing=True)
+
+	def test_single_history_per_member(self):
+		"""Test that only one history record exists per member"""
+		member_name = "test-member-1"
+
+		# Create first history record
+		history1 = LibraryMemberHistory.get_or_create_history(member_name)
+		self.assertIsNotNone(history1)
+
+		# Try to get history again - should return the same record
+		history2 = LibraryMemberHistory.get_or_create_history(member_name)
+		self.assertEqual(history1.name, history2.name)
+
+	def test_add_multiple_transactions(self):
+		"""Test adding multiple transaction lines to the same member"""
+		member_name = "test-member-1"
+
+		# Add first transaction
+		LibraryMemberHistory.add_transaction_to_history(
+			member_name=member_name,
+			article="Book 1",
+			author="Author 1",
+			transaction_status="Issued"
+		)
+
+		# Add second transaction
+		LibraryMemberHistory.add_transaction_to_history(
+			member_name=member_name,
+			article="Book 2",
+			author="Author 2",
+			transaction_status="Returned"
+		)
+
+		# Verify only one history record exists with two transaction lines
+		history_records = frappe.get_all("Library Member History",
+			filters={"member_name": member_name})
+		self.assertEqual(len(history_records), 1)
+
+		history_doc = frappe.get_doc("Library Member History", history_records[0].name)
+		self.assertEqual(len(history_doc.transaction_history), 2)
